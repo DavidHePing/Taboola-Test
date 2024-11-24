f9668c2b9271e1afa22c76286d2caaddd1fa4666
Dav He <s991179@gmail.com>
2024-11-24 21:47:12 +0800

    chore: add ./api for helm chart

--- api/run.sh
+++ api/run.sh
@@ -7,7 +7,7 @@ ImageName="$DockerHubUrl/springboot-test1:${Tag}"
 (
     docker pull $ImageName
 
-    helm upgrade --install springboot-test1 helm-chart/ \
+    helm upgrade --install springboot-test1 ./api/helm-chart/ \
     --set service.nodePort=8080 \
     --set image.repository=$ImageName \
     --set fullnameOverride=springboot-test1
