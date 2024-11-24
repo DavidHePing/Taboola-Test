78333f1fa47e903cbffd56253939063fe7779de7
Dav He <s991179@gmail.com>
2024-11-24 22:10:30 +0800

    chore: set node port 30080

--- api/run.sh
+++ api/run.sh
@@ -8,7 +8,7 @@ ImageName="$DockerHubUrl/springboot-test1:${Tag}"
     docker pull $ImageName
 
     helm upgrade --install springboot-test1 ./api/helm-chart/ \
-    --set service.nodePort=8080 \
+    --set service.nodePort=30080 \
     --set image.repository=$ImageName \
     --set fullnameOverride=springboot-test1
 
