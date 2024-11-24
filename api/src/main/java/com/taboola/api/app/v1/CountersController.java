9203e00d71cca2bd2f4ec41b427f03f513b9ee4a
Dav He <s991179@gmail.com>
2024-11-24 18:11:30 +0800

    chore: add swagger

--- api/src/main/java/com/taboola/api/app/v1/CountersController.java
+++ api/src/main/java/com/taboola/api/app/v1/CountersController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController()
-@RequestMapping("/v1/api/counters")
+@RequestMapping("/api/counters")
 public class CountersController {
     private final ICounterService counterService;
 
